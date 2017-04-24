fc4dad34365cbd509ba8aaae57ae8a4551575a27
Flávio Veloso <flaviovs@magnux.com>
2017-04-24 14:41:00 -0700

    Fix Arith*Op not checking/returning floats properly

--- src/ArithBinaryOp.php
+++ src/ArithBinaryOp.php
@@ -4,8 +4,8 @@ namespace PHPAST;
 
 abstract class ArithBinaryOp extends BinaryOp {
 	public function arithPromote($value, Literal $lit1, Literal $lit2) {
-		if ($lit2 instanceof Float || $lit2 instanceof Float) {
-			new Float($value, $this->label);
+		if ($lit1 instanceof Float || $lit2 instanceof Float) {
+			return new Float($value, $this->label);
 		}
 		return new Integer($value, $this->label);
 	}
--- src/ArithUnaryOp.php
+++ src/ArithUnaryOp.php
@@ -5,7 +5,7 @@ namespace PHPAST;
 abstract class ArithUnaryOp extends UnaryOp {
 	public function arithPromote($value, Literal $lit) {
 		if ($lit instanceof Float) {
-			new Float($value, $this->label);
+			return new Float($value, $this->label);
 		}
 		return new Integer($value, $this->label);
 	}
