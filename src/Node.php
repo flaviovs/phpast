369c9cab1e4bca1acc6c1170a357fb4f6a68ad4f
Flávio Veloso <flaviovs@magnux.com>
2017-05-19 17:16:51 -0700

    Add Node->checkType()

--- src/Node.php
+++ src/Node.php
@@ -23,4 +23,16 @@ abstract class Node {
 		throw new TypeException($this->label,
 		                        get_class($this) . ' has no value');
 	}
+
+	protected function checkType(Node $node, $class, $message = NULL) {
+		if (!($node instanceof $class)) {
+			if (!$message) {
+				// Remove our namespace prefix from the class, if any.
+				$class = preg_replace('~^' . preg_quote(__NAMESPACE__) . '\\\~',
+				                      '', $class);
+				$message = "Expecting $class, got " . get_class($node);
+			}
+			throw new TypeException($this->label, $message);
+		}
+	}
 }
